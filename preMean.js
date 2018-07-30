3f565c4c5256bd4d84903f6099d37385c7478035
katisari <39178730+katisari@users.noreply.github.com>
2018-07-30 10:10:48 -0700

    Update preMean.js

--- preMean.js
+++ preMean.js
@@ -1,21 +1,21 @@
-function maxMinAvg(arr) {
-    var sum = 0
-    var count = 0
-    var max = arr[0]
-    var min = arr[0]
-    for (var i = 0; i < arr.length; ++i) {
-        if (arr[i] < min) {
-            min = arr[i]
-        } else if (arr[i] > max) {
-            max = arr[i]
-        }
-        count += 1
-        sum += arr[i]
-    }
-    return("The minimum is " + min + ". Max is " + max + ". Avg is " + sum/count)
-}
+// function maxMinAvg(arr) {
+//     var sum = 0
+//     var count = 0
+//     var max = arr[0]
+//     var min = arr[0]
+//     for (var i = 0; i < arr.length; ++i) {
+//         if (arr[i] < min) {
+//             min = arr[i]
+//         } else if (arr[i] > max) {
+//             max = arr[i]
+//         }
+//         count += 1
+//         sum += arr[i]
+//     }
+//     return("The minimum is " + min + ". Max is " + max + ". Avg is " + sum/count)
+// }
 
-maxMinAvg([1, -2, 9, 4])
+// maxMinAvg([1, -2, 9, 4])
 
 
 
@@ -25,33 +25,50 @@ maxMinAvg([1, -2, 9, 4])
 // If the number is divisible by 3 but not by 5, log "Fizz" instead of the number
 // If the number is divisible by 5 but not by 3, log "Buzz" instead of the number
 
-function fizzbuzz(num) {
-    var return_str = ""
-    if (!(num.isInteger) || (num <= 0)) {
-  //     console.log("num must be positive number")
-      return "num must be positive number"
-    }
-    for (var i = 1; i <= num; ++i) {
-      if (i % 3 == 0 && i % 5 == 0) {
-        return_str += " FizzBuzz ";
+// function fizzbuzz(num) {
+//     var return_str = ""
+//     if (!(num.isInteger) || (num <= 0)) {
+//   //     console.log("num must be positive number")
+//       return "num must be positive number"
+//     }
+//     for (var i = 1; i <= num; ++i) {
+//       if (i % 3 == 0 && i % 5 == 0) {
+//         return_str += " FizzBuzz ";
         
-      } else if (i % 3 == 0) {
-        return_str += " Fizz ";
+//       } else if (i % 3 == 0) {
+//         return_str += " Fizz ";
         
-      } else if (i % 5 == 0) {
-        return_str += " Buzz ";
-      } else {
-        return_str += i + " ";
-      }
-    }
-    return return_str
+//       } else if (i % 5 == 0) {
+//         return_str += " Buzz ";
+//       } else {
+//         return_str += i + " ";
+//       }
+//     }
+//     return return_str
     
-  }
+//   }
   
-  console.log(fizzbuzz(15))
+//   console.log(fizzbuzz(15))
 
 
 
-  function bracesValid(brace_comb) {
-      
+function bracesValid(brace_comb) {
+  var other_pair = {"}": "{", ")": 
+  "(", "]": "["}
+  var stack = []
+  for (var i = 0; i < brace_comb.length; ++i) {
+    if (brace_comb[i] == "{" || brace_comb[i] == "("
+        || brace_comb[i] == "[") {
+      stack.push(brace_comb[i])
+    } 
+    else if (stack[stack.length-1] === other_pair[brace_comb[i]]) {
+      stack.pop()
+    }
+    else {
+      return false
+    }
   }
+  return true
+}
+
+bracesValid("{{()}}[]")
